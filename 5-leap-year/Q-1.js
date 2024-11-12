9626835bf15bc879b409a4322a1c1ac74b854a50
Ashutosh Pathak <ashutoshp887@gmail.com>
2024-11-12 22:37:54 +0530

    add file code

---
+++ 5-leap-year/Q-1.js
@@ -0,0 +1,10 @@
+/**
+ * Write a function that tells if provided year is a leap year or not
+ * Input: A number , positive integer -> 1900,2023
+ * Return : true if leap year , false if not leap year
+ * leap year :
+ * Every 4th year is a leap year 
+ * Leap year has 366 days 
+ * Normal year has 365 days
+ * February has 28 days or 29 days 
+ */
