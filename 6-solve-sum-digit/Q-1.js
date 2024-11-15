2c9a8ca8e40720518f8975ba2d4c0253b71ba70c
Ashutosh Pathak <ashutoshp887@gmail.com>
2024-11-15 18:32:55 +0530

    complete

--- 6-solve-sum-digit/Q-1.js
+++ 6-solve-sum-digit/Q-1.js
@@ -5,3 +5,21 @@
  * Return: sum of all digits
  * Example: 12 : 1+2 =3
  */
+
+
+function sum(input){
+    if(input <1){
+        throw new Error("use positive number")
+        
+    }
+    const string = input.toString();
+    const split1 = string.split('')
+    
+    let final =0;
+split1.forEach(num=>{
+ 
+    final = final + parseInt(num)
+   })
+   return final;
+}
+console.log(sum(56))
