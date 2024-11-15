2af1c5d7ece7564917c15e426e56f37af4d9b0c5
Ashutosh Pathak <ashutoshp887@gmail.com>
2024-11-15 18:03:03 +0530

    new file

---
+++ 6-solve-sum-digit/Q-1.js
@@ -0,0 +1,7 @@
+/**
+ * Write a function that calcualtes and prints the sum of the digits of
+ * a given number
+ * Input: A number,positive integer-> 12,34,123,1234
+ * Return: sum of all digits
+ * Example: 12 : 1+2 =3
+ */
