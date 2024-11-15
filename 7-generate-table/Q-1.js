5b4d846eac76ad8223fc575dd7567fe4a394a9b7
Ashutosh Pathak <ashutoshp887@gmail.com>
2024-11-15 19:01:34 +0530

    new

---
+++ 7-generate-table/Q-1.js
@@ -0,0 +1,10 @@
+/**
+ * Write a function that generates and prints a multiplication table for a 
+ * given number up to a specified range
+ * Input : 2 Positive Number
+ *   Table
+ *   Table till
+ * Return: Nothing
+ * Example:
+ * 4,10 => 4, 8,12 .....40 
+ */
